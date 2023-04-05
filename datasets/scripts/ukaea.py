030f7a5f3bb4d61f97563f1ae710e31f8e96f19b
Alexander Mead <alexander@digilab.co.uk>
2023-04-05 17:09:57 +0100

    updated with test data

--- datasets/scripts/ukaea.py
+++ datasets/scripts/ukaea.py
@@ -3,9 +3,11 @@ import pandas as pd
 
 # Parameters
 seed = 123
-n = 100
-infile = 'datasets/ukaea.csv'
-outfile = 'datasets/ukaea_small.csv'
+n_small = 100
+infile = "datasets/ukaea.csv"
+outfile_small = "datasets/ukaea_small.csv"
+n_test = 5
+outfile_test = "campaigns/ukaea/test.csv"
 
 # Set random seed
 np.random.seed(seed)
@@ -13,6 +15,10 @@ np.random.seed(seed)
 # Read in full data
 df = pd.read_csv(infile)
 
-# Subsample and save
-df_small = df.sample(n, replace=False)
-df_small.to_csv(outfile, index=False)
+# Subsample and save smaller dataframe
+df_small = df.sample(n_small, replace=False)
+df_small.to_csv(outfile_small, index=False)
+
+# Test dataframe
+df_test = df[-n_test:]
+df_test.to_csv(outfile_test, index=False)

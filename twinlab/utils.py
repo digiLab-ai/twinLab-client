8cb73a0b6f252d621d6f9bcc7146d800b75021e1
Alexander Mead <alexander@digilab.co.uk>
2023-05-24 14:36:16 +0100

    added dev server

--- twinlab/utils.py
+++ twinlab/utils.py
@@ -14,10 +14,13 @@ from .settings import ENV
 
 PARAMS_COERCION = {  # Convert these names in the params file
     "test_train_split": "train_test_split",  # Common mistake
-    # "num_train_examples": "train_test_split", # TODO: Think of something better
+    "num_training_examples": "train_test_split",  #  TODO: Think of something better
+    "dataset": "filename",
 }
-TRAIN_CAMPAIGN_CLOUD_URL = "https://cma567qwquixu7bbjcnhbjsxjm0yumvu.lambda-url.eu-west-2.on.aws/"
+TRAIN_CAMPAIGN_DEV_URL = "https://pdersvjxmgrkqojwyeyocqm7le0iwtkx.lambda-url.eu-west-2.on.aws/"
 TRAIN_CAMPAIGN_STAGE_URL = "https://b7vgjlsn73e7n7kci5rwoxjc7e0pkcqn.lambda-url.eu-west-2.on.aws/"
+TRAIN_CAMPAIGN_CLOUD_URL = "https://cma567qwquixu7bbjcnhbjsxjm0yumvu.lambda-url.eu-west-2.on.aws/"
+
 
 ### Utility functions ###
 
@@ -71,13 +74,16 @@ def get_server_url(server: str) -> str:
     """
     if server == "local":
         baseURL = ENV.TWINLAB_LOCAL_SERVER
+    elif server == "dev":
+        baseURL = ENV.TWINLAB_DEV_SERVER
     elif server == "stage":
         baseURL = ENV.TWINLAB_STAGE_SERVER
     elif server == "cloud":
         baseURL = ENV.TWINLAB_SERVER
     else:
         print("Server:", server)
-        raise ValueError("Server must be either 'local', 'cloud', or 'stage'")
+        raise ValueError(
+            "Server must be either 'local', 'dev', 'stage', or 'cloud'")
     if baseURL is None:  # Catch if the server URL has not been set
         raise ValueError("Server URL not set")
     return baseURL
@@ -88,10 +94,12 @@ def get_train_campaign_url(server: str) -> str:
     Get the URL for the train_campaign lambda function
     These are different to avoid the AWS Lambda 29s gateway timeout
     """
-    if server == "cloud":
-        url = TRAIN_CAMPAIGN_CLOUD_URL
+    if server == "dev":
+        url = TRAIN_CAMPAIGN_DEV_URL
     elif server == "stage":
         url = TRAIN_CAMPAIGN_STAGE_URL
+    elif server == "cloud":
+        url = TRAIN_CAMPAIGN_CLOUD_URL
     else:
         url = get_server_url(server) + "/train_campaign"
     return url
